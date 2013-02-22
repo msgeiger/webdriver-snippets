4cd9f6df813ae9d25881bcfb1b0bb29f5f98cb7b
Mark Geiger <msgeiger@gmail.com>
2013-02-21 19:21:52 -0500

    Create test_links.rb

---
+++ test_links.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/build_config.rb'
+require File.dirname(__FILE__) + '/test_unit_config.rb'
+
+# Instantiate custom Datanex handler
+class TestLinks < Geiger 
+  def test_links
+    driver = self.get_driver
+    wait = self.get_wait
+    testing_url = 'http://www.marinebay.com'
+
+    url = [
+      "#{testing_url}/browse/boats",
+      "#{testing_url}/browse/boatengines",
+      "#{testing_url}/browse/trailers"
+    ]
+   
+   ids = [
+     {:id => "title_navigation"},
+     {:id => "main_title"}
+   ]
+
+    url.each do |page|
+      driver.navigate.to page 
+      ids.each do |element|
+        self.wait_for_element element 
+        puts element
+      end
+    end
+  end
+end
